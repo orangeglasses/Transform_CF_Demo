370efae39a38f65835773a9e4546f36891858606
Martin Caarels <martin@caarels.com>
2017-11-03 22:43:08 +0100

    added list of bots

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const CF_INSTANCE_GUID = process.env.CF_INSTANCE_GUID
 const MEMORY_LIMIT = process.env.MEMORY_LIMIT
 const sys = require('util')
 const exec = require('child_process').exec;
+const bots = require('./autobots.json')
 
 app.use(express.static(path.join(__dirname, 'assets')))
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -17,7 +18,8 @@ app.get('/', function (req, res) {
   res.render('home', {
     CF_INSTANCE_INDEX: CF_INSTANCE_INDEX,
     CF_INSTANCE_GUID: CF_INSTANCE_GUID,
-    MEMORY_LIMIT, MEMORY_LIMIT
+    MEMORY_LIMIT: MEMORY_LIMIT,
+    BOTS: bots
   })
     })
 
