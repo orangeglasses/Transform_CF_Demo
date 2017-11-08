ed546cfa7963c316dbf6a60c72ed188bc6c7de77
Martin Caarels <martin@caarels.com>
2017-11-08 17:07:19 +0100

    removed debugging

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # set tracing env var to true for more verbosity - set by martin caarels oct 5 2017
-set
-echo "api:  $api \n usename: $username \t org:  $organization \n space:  $space"
+# set
 cf login -a $api -u $username -p $password -o $organization -s $space &&\
 cd ./resource-git/ &&\
 cf push -f $manifest
