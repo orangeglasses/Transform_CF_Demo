dd1f06381bc7ffcec6e0788abfd5520408c0486a
Martin Caarels <martin@caarels.com>
2017-11-08 16:48:08 +0100

    terminate strings dude

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,7 +2,7 @@
 
 # set tracing env var to true for more verbosity - set by martin caarels oct 5 2017
 set
-echo "api:  $api \n usename: $username \t org:  $organization \n space:  $space
+echo "api:  $api \n usename: $username \t org:  $organization \n space:  $space"
 cf login -a $api -u $username -p $password -o $organization -s $space &&\
 cd ./resource-git/ &&\
 cf push -f $manifest
